a61998807c7afa5bfea525d54be672772cbc0368
Piyush Vinchurkar <piyushvinchurkar10@gmail.com>
2020-03-31 17:10:37 +0530

    front directory, menu item page with css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      Hiii
+      <HomePage />
     </div>
   );
 }
